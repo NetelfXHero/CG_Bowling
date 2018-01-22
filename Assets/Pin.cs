82da4cea68bd1cc9d205708ddb39064d38b5ed27
Alexander Brevig <alexanderbrevig@gmail.com>
2018-01-22 14:14:40 +0100

    added hit sound for first pin in PinFormation

---
+++ Assets/Pin.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pin : MonoBehaviour {
+    public AudioClip hitSound;
+    public AudioSource audioSource;
+
+    // Use this for initialization
+    private void OnTriggerEnter(Collider other)
+    {
+        // check to see if we collided with the Ball
+        if (other.GetComponent<Ball>() != null)
+        {
+            audioSource.PlayOneShot(hitSound);
+        }
+    }
+}
