36aa2bd5e5773ebeb6c29d681f901bd4632f1000
Alexander Brevig <alexanderbrevig@gmail.com>
2018-01-22 13:58:14 +0100

    new shot after ball stops

--- Assets/Ball.cs
+++ Assets/Ball.cs
@@ -3,18 +3,24 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Ball : MonoBehaviour {
+    public GameObject BowlingPrefab;
     public Aimer aimer;
     public Camera cam;
     public float aimDistance;
     public float maxPower;
     public float jumpForce;
+    public float startedMove;
 
     public bool IsMoving { get; private set; }
 
     // Use this for initialization fffffffffffffffffffffffffffffffffffffffff
 	void Start () {
         cam = FindObjectOfType<Camera>();
-	}
+        startedMove = float.MaxValue;
+
+        aimer.gameObject.SetActive(true);
+        aimer.transform.position = new Vector3(0, 0.2f, 0);
+    }
 	
 	// Update is called once per frame
 	void Update () {
@@ -34,6 +40,16 @@ public class Ball : MonoBehaviour {
             var targetPos = transform.position + distance;
             aimer.transform.position = new Vector3(targetPos.x, aimer.transform.position.y, targetPos.z);
         }
+        
+        if (IsMoving 
+            && GetComponent<Rigidbody>().velocity.magnitude < 0.2f
+            && Time.time - startedMove > 1)
+        {
+            var ball = Instantiate(BowlingPrefab, transform.position, Quaternion.identity);
+            var gc = GameObject.FindObjectOfType<GameController>();
+            gc.ball = ball.GetComponent<Ball>();
+            Destroy(gameObject); //delete from scene!
+        }
     }
 
     public void Throw(float power)
@@ -44,6 +60,7 @@ public class Ball : MonoBehaviour {
         direction *= power * maxPower;
         GetComponent<Rigidbody>().AddForce(direction, ForceMode.Impulse);
         IsMoving = true;
+        startedMove = Time.time; //Time.time is seconds since game launch
     }
 
     public void Jump()
